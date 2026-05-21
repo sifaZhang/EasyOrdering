9ed56b8032d84dc8eb395c1d9ea053628d80b32f
sifaz <sifazhang.nz@gmail.com>
2026-05-21 16:54:32 +1200

    add doc + shift mysql to protgre

--- dbConfig.js
+++ dbConfig.js
@@ -1,19 +1,20 @@
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',        
-    database: 'easyordering',
-    dateStrings: true       
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-conn.connect(function(err) {
-    if (err) {
-        console.error('Error connecting to the database: ' + err.stack);
-        return;
-    }
-    console.log('Connected to the database as id ' + conn.threadId);
-});
+// 测试连接（PostgreSQL 正确写法）
+pool.connect()
+  .then(client => {
+    console.log('Connected to the database');
+    client.release(); // 释放连接
+  })
+  .catch(err => {
+    console.error('Database connection error:', err.stack);
+  });
 
-
-module.exports = conn;
+module.exports = pool;
